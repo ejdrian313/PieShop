b480a7133aba71d14272bfe36a371cfcd2b81a6f
Adrian Kujawski <adrian.kujawski.313@gmail.com>
2017-12-24 14:33:11 +0100

    Add ShoppingCart controllers, views and cart

--- Controllers/ShoppingCartController.cs
+++ Controllers/ShoppingCartController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
 
 namespace PieShop.Controllers
 {
     public class ShoppingCartController : Controller
     {
-        public IActionResult Index()
+        private readonly IPieRepository _pieRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IPieRepository pieRepository, ShoppingCart shoppingCart)
         {
-            return View();
+            _pieRepository = pieRepository;
+            _shoppingCart = shoppingCart;
         }
     }
 }
